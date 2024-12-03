3db3c4b1574e032f92847a92887640f652ee4b54
notTGY <microcraft47@gmail.com>
2024-12-03 10:38:05 +0300

    day 3

---
+++ day3-2.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "strings"
+  "strconv"
+)
+
+func abs(x, y int) (int) {
+  if x > y {
+    return x - y
+  }
+  return y - x
+}
+
+func main() {
+  data, _ := os.ReadFile("day3.input")
+
+  s := string(data)
+
+  total := 0
+  for strings.Index(s, "mul(") != -1 {
+    //fmt.Printf("(: %s\n", s)
+    j := strings.Index(s, "mul(")
+    k := strings.Index(s, "don't()")
+
+    if j > k && k != -1 {
+      s = string(s[k+5:])
+      k = strings.Index(s, "do()")
+      if k == -1 {
+        s = ""
+        continue
+      }
+      s = string(s[k+4:])
+    }
+
+    j = strings.Index(s, "mul(")
+    s = string(s[j+4:])
+
+    //fmt.Printf(",: %s\n", s)
+    j = strings.Index(s, ",")
+    // 3-max int, 4-len("mul(")
+    if j > 3+4 {
+      continue
+    }
+    aStr := string(s[:j])
+    //fmt.Printf("aStr: %s\n", aStr)
+    if len(aStr) > len(strings.TrimSpace(aStr)) {
+      continue
+    }
+    a, err := strconv.Atoi(aStr)
+    if err != nil {
+      continue
+    }
+    s = string(s[j+1:])
+
+    //fmt.Printf("): %s\n", s)
+    j = strings.Index(s, ")")
+    bStr := string(s[:j])
+    //fmt.Printf("bStr: %s\n", bStr)
+    if len(bStr) > len(strings.TrimSpace(bStr)) {
+      continue
+    }
+    if len(bStr) > 3 {
+      continue
+    }
+    b, err := strconv.Atoi(bStr)
+    if err != nil {
+      continue
+    }
+    s = string(s[j:])
+    //fmt.Printf("a: %d; b: %d\n", a, b)
+    total += a*b
+  }
+
+  fmt.Printf("\ntotal: %d\n", total)
+}
