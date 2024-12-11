af461037f17b26d395ac06f73c7e31948d8931cd
notTGY <microcraft47@gmail.com>
2024-12-11 10:52:20 +0300

    day 11 prep

---
+++ day12-1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "strings"
+)
+
+
+func main() {
+  data, _ := os.ReadFile("day10.input")
+  lines := strings.Split(string(data), "\n")
+  lines = lines[:len(lines)-1]
+
+  nrows := len(lines)
+  ncols := len(lines[0])
+
+
+  total := 0
+
+  fmt.Printf("total: %d\n", total)
+}
+
