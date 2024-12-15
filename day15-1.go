409b4ab19cdd6aca1c7aa3d3df61c6022d027e24
notTGY <microcraft47@gmail.com>
2024-12-16 01:41:13 +0300

    day 15

--- day15-1.go
+++ day15-1.go
@@ -6,15 +6,186 @@ import (
 	"strings"
 )
 
+func printMap(
+  robot [2]int,
+  boxes [][2]int,
+  edges [][2]int,
+) {
+  nrows := 0
+  ncols := 0
+  for _, edge := range edges {
+    if edge[0]+1 > ncols {
+      ncols = edge[0]+1
+    }
+    if edge[1]+1 > nrows {
+      nrows = edge[1]+1
+    }
+  }
+
+  var m[][]rune
+  for row := 0; row < nrows; row++ {
+    m = append(m, []rune{})
+    for col := 0; col < ncols; col++ {
+      m[row] = append(m[row], '.')
+    }
+  }
+
+  for _, edge := range edges {
+    m[edge[1]][edge[0]] = '#'
+  }
+  for _, box := range boxes {
+    m[box[1]][box[0]] = 'O'
+  }
+  m[robot[1]][robot[0]] = '@'
+
+  for row := 0; row < nrows; row++ {
+    for col := 0; col < ncols; col++ {
+      fmt.Printf("%s", string(m[row][col]))
+    }
+    fmt.Printf("\n")
+  }
+}
+
+func move(
+  robot [2]int,
+  boxes [][2]int,
+  edges [][2]int,
+  inc int,
+  axis int,
+) ([2]int, [][2]int) {
+  // X=col=[0]
+  along := 0
+  ortogonal := 1
+
+  // Y=row=[1]
+  if axis == 1 {
+    along = 1
+    ortogonal = 0
+  }
+
+  edge_along_val := -1
+  for _, edge := range edges {
+    along_val := edge[along]
+    if edge[ortogonal] == robot[ortogonal] {
+      if along_val*inc > inc*robot[along] {
+        if edge_along_val == -1 ||
+          edge_along_val*inc > inc*along_val {
+          edge_along_val = along_val
+        }
+      }
+    }
+  }
+
+  //fmt.Printf("edge: %v\n", edges[edgeIdx])
+
+  var relevant []int
+  var new_boxes [][2]int
+  for i, box := range boxes {
+    if box[ortogonal] == robot[ortogonal] &&
+      box[along]*inc > inc*robot[along] &&
+      box[along]*inc < inc*edge_along_val {
+      relevant = append(relevant, i)
+    } else {
+      new_boxes = append(
+        new_boxes,
+        [2]int{box[0], box[1]},
+      )
+    }
+  }
+
+  //fmt.Printf("relevant: %d\n", len(relevant))
+
+  spaceCoord := -1
+  for coord := robot[along]+inc;
+    coord*inc < inc*edge_along_val;
+    coord += inc {
+    //fmt.Printf("coord: %d\n", coord)
+    filled := false
+    for _, relIdx := range relevant {
+      filled = filled || (boxes[relIdx][along] == coord)
+    }
+    if !filled {
+      spaceCoord = coord
+      break
+    }
+  }
+  //fmt.Printf("space: %d\n", spaceCoord)
+
+  if spaceCoord != -1 {
+    robot[along] += inc
+  }
+  for _, relIdx := range relevant {
+    old_box := boxes[relIdx]
+    old_along := old_box[along]
+    if spaceCoord != -1 &&
+      old_along*inc < inc*spaceCoord {
+      old_along += inc
+    }
+
+    new_box := [2]int { old_box[0], old_box[1] }
+    new_box[along] = old_along
+
+    new_boxes = append(new_boxes, new_box)
+  }
+
+  return robot, new_boxes
+}
+
 func main() {
 	data, _ := os.ReadFile("day15.input")
-	lines := strings.Split(string(data), "\n")
-	lines = lines[:len(lines)-1]
+	blocks := strings.Split(string(data), "\n\n")
+  room_map := strings.Split(blocks[0], "\n")
+  motions := strings.Split(blocks[1], "\n")
+	motions = motions[:len(motions)-1]
 
-	nrows := len(lines)
-	ncols := len(lines[0])
+  var robot [2]int
+  var boxes [][2]int
+  var edges [][2]int
+
+  for row, line := range room_map {
+    for col, cell := range line {
+      coord := [2]int{col, row}
+      switch cell {
+        case '#':
+          edges = append(edges, coord)
+        case 'O':
+          boxes = append(boxes, coord)
+        case '@':
+          robot = coord
+      }
+    }
+  }
+
+  for _, motion_group := range motions {
+    for _, movement := range motion_group {
+      switch movement {
+        case '>':
+          robot, boxes = move(
+            robot, boxes, edges, 1, 0,
+          )
+        case '<':
+          robot, boxes = move(
+            robot, boxes, edges, -1, 0,
+          )
+        case '^':
+          robot, boxes = move(
+            robot, boxes, edges, -1, 1,
+          )
+        case 'v':
+          robot, boxes = move(
+            robot, boxes, edges, 1, 1,
+          )
+      }
+      //fmt.Printf("Move %s:\n", string(movement))
+      //printMap(robot, boxes, edges)
+      //fmt.Printf("\n")
+    }
+  }
 
 	total := 0
+  for _, box := range boxes {
+    total += 100*box[1] + box[0]
+  }
 
 	fmt.Printf("total: %d\n", total)
 }
---
+++ day15-2.go
@@ -0,0 +1,230 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+  "slices"
+)
+
+func printMap(
+  robot [2]int,
+  boxes [][2]int,
+  edges [][2]int,
+) {
+  nrows := 0
+  ncols := 0
+  for _, edge := range edges {
+    if edge[0]+2 > ncols {
+      ncols = edge[0]+2
+    }
+    if edge[1]+1 > nrows {
+      nrows = edge[1]+1
+    }
+  }
+
+  var m[][]rune
+  for row := 0; row < nrows; row++ {
+    m = append(m, []rune{})
+    for col := 0; col < ncols; col++ {
+      m[row] = append(m[row], '.')
+    }
+  }
+
+  for _, edge := range edges {
+    m[edge[1]][edge[0]] = '#'
+    m[edge[1]][edge[0]+1] = '#'
+  }
+  for _, box := range boxes {
+    m[box[1]][box[0]] = '['
+    m[box[1]][box[0]+1] = ']'
+  }
+  m[robot[1]][robot[0]] = '@'
+
+  for row := 0; row < nrows; row++ {
+    for col := 0; col < ncols; col++ {
+      fmt.Printf("%s", string(m[row][col]))
+    }
+    fmt.Printf("\n")
+  }
+}
+
+func collider(
+  obj [2]int,
+  box [2]int,
+  obj_size int,
+) bool {
+  if obj[1] != box[1] {
+    return false
+  }
+  intersect_1 := obj[0] == box[0] || obj[0] == box[0]+1
+  intersect_2 := obj[0]+1 == box[0] || obj[0]+1 == box[0]+1
+  if obj_size == 2 {
+    return intersect_1 || intersect_2
+  }
+
+  return intersect_1
+}
+
+func canMove(
+  obj [2]int,
+  boxes [][2]int,
+  edges [][2]int,
+  inc int,
+  axis int,
+  obj_size int,
+) (bool, []int) {
+  relevant := []int{}
+
+  new_obj := [2]int {obj[0], obj[1]}
+  new_obj[axis] += inc
+
+  for _, edge := range edges {
+    collides := collider(
+      new_obj,
+      edge,
+      obj_size,
+    )
+    if collides {
+      return false, relevant
+    }
+  }
+
+  colliding := []int{}
+  for i, box := range boxes {
+    collides := collider(
+      new_obj,
+      box,
+      obj_size,
+    )
+    if collides {
+      colliding = append(colliding, i)
+      relevant = append(relevant, i)
+    }
+  }
+  if len(colliding) == 0 {
+    return true, relevant
+  }
+
+  for _, i := range colliding {
+    box := boxes[i]
+    boxes_wo := [][2]int{}
+    for j, box := range boxes {
+      if i != j {
+        boxes_wo = append(boxes_wo, box)
+      }
+    }
+    can, rel := canMove(box, boxes_wo, edges, inc, axis, 2)
+    if !can {
+      return false, relevant
+    }
+    for _, i := range rel {
+      if slices.Index(relevant, i) == -1 {
+        relevant = append(relevant, i)
+      }
+    }
+  }
+
+  return true, relevant
+}
+
+func move(
+  robot [2]int,
+  boxes [][2]int,
+  edges [][2]int,
+  inc int,
+  axis int,
+) ([2]int, [][2]int) {
+  can, relevant := canMove(
+    robot,
+    boxes,
+    edges,
+    inc,
+    axis,
+    1,
+  )
+  if !can {
+    return robot, boxes
+  }
+  //fmt.Printf("relevant: %d\n", len(relevant))
+  var new_boxes [][2]int
+  for _, box := range boxes {
+    new_box := [2]int { box[0], box[1] }
+    new_boxes = append(new_boxes, new_box)
+  }
+  for _, i := range relevant {
+    new_boxes[i][axis] += inc
+  }
+  new_robot := [2]int { robot[0], robot[1] }
+  new_robot[axis] += inc
+  return new_robot, new_boxes
+}
+
+func main() {
+	data, _ := os.ReadFile("day15.test")
+	blocks := strings.Split(string(data), "\n\n")
+  room_map := strings.Split(blocks[0], "\n")
+  motions := strings.Split(blocks[1], "\n")
+	motions = motions[:len(motions)-1]
+
+  var robot [2]int
+  var boxes [][2]int
+  var edges [][2]int
+
+  for row, line := range room_map {
+    for col, cell := range line {
+      coord := [2]int{((col+1)*2)-2, row}
+      switch cell {
+        case '#':
+          edges = append(edges, coord)
+        case 'O':
+          boxes = append(boxes, coord)
+        case '@':
+          robot = coord
+      }
+    }
+  }
+
+  DEBUG := true
+  if DEBUG {
+    fmt.Printf("Initial state:\n")
+    printMap(robot, boxes, edges)
+    fmt.Printf("\n")
+  }
+  for _, motion_group := range motions {
+    for _, movement := range motion_group {
+      switch movement {
+        case '>':
+          robot, boxes = move(
+            robot, boxes, edges, 1, 0,
+          )
+        case '<':
+          robot, boxes = move(
+            robot, boxes, edges, -1, 0,
+          )
+        case '^':
+          robot, boxes = move(
+            robot, boxes, edges, -1, 1,
+          )
+        case 'v':
+          robot, boxes = move(
+            robot, boxes, edges, 1, 1,
+          )
+      }
+      if DEBUG {
+        fmt.Printf("Move %s:\n", string(movement))
+        printMap(robot, boxes, edges)
+        fmt.Printf("\n")
+      }
+    }
+  }
+
+	total := 0
+  for _, box := range boxes {
+    total += 100*box[1] + box[0]
+  }
+
+	fmt.Printf("total: %d\n", total)
+}
+
+// 1451191 - too high
