a7b9acb3969e8b7b5b6d16d49de9e7cc6a4c932f
notTGY <microcraft47@gmail.com>
2024-12-17 11:47:11 +0300

    day 17

--- day17-1.go
+++ day17-1.go
@@ -7,25 +7,156 @@ import (
 	"strings"
 )
 
-func get_input() ([]string) {
+func get_input() ([]int, []int) {
 	data, _ := os.ReadFile("day17.input")
-	lines := strings.Split(string(data), "\n")
-	lines = lines[:len(lines)-1]
-	return lines
+  blocks := strings.Split(string(data), "\n\n")
+  registerStrs := strings.Split(blocks[0], "\n")
+  registers := []int{}
+  for _, s := range registerStrs {
+    splitted := strings.Split(s, ": ")
+    x, _ := strconv.Atoi(splitted[1])
+    registers = append(registers, x)
+  }
+
+  progStr := strings.Split(blocks[1], "\n")[0]
+  progList := strings.Split(progStr, ": ")[1]
+  program := []int{}
+  for _, ps := range strings.Split(progList, ",") {
+    x, _ := strconv.Atoi(ps)
+    program = append(program, x)
+  }
+	return registers, program
 }
 
-func get_test() ([]string) {
+func get_test() ([]int, []int) {
 	data, _ := os.ReadFile("day17.test")
-	lines := strings.Split(string(data), "\n")
-	lines = lines[:len(lines)-1]
-	return lines
+  blocks := strings.Split(string(data), "\n\n")
+  registerStrs := strings.Split(blocks[0], "\n")
+  registers := []int{}
+  for _, s := range registerStrs {
+    splitted := strings.Split(s, ": ")
+    x, _ := strconv.Atoi(splitted[1])
+    registers = append(registers, x)
+  }
+
+  progStr := strings.Split(blocks[1], "\n")[0]
+  progList := strings.Split(progStr, ": ")[1]
+  program := []int{}
+  for _, ps := range strings.Split(progList, ",") {
+    x, _ := strconv.Atoi(ps)
+    program = append(program, x)
+  }
+	return registers, program
+}
+
+func adv(registers []int, operand int) int {
+  num := registers[0]
+  denom := 1 << operand
+
+  return num / denom
+}
+
+func bxl(registers []int, operand int) int {
+  return registers[1] ^ operand
+}
+
+func bst(registers []int, operand int) int {
+  return operand % 8
+}
+
+func bxc(registers []int) int {
+  return registers[1] ^ registers[2]
+}
+
+func combo(registers[]int, operand int) int {
+  if operand < 4 {
+    return operand
+  }
+  return registers[operand - 4]
+}
+
+func checkQuine(out []int, program []int) bool {
+  if len(out) != len(program) {
+    return false
+  }
+  for i, o := range out {
+    if program[i] != o {
+      return false
+    }
+  }
+  return true
+}
+
+func eval(registers []int, program []int) ([]int) {
+  out := []int{}
+
+  for ip := 0; ip < len(program); {
+    switch program[ip] {
+      case 0:
+        registers[0] = adv(
+          registers,
+          combo(registers, program[ip+1]),
+        )
+      case 1:
+        registers[1] = bxl(
+          registers,
+          program[ip+1],
+        )
+      case 2:
+        registers[1] = bst(
+          registers,
+          combo(registers, program[ip+1]),
+        )
+      case 3:
+        if registers[0] != 0 {
+          ip = program[ip+1]
+          continue
+        }
+      case 4:
+        registers[1] = bxc(
+          registers,
+        )
+      case 5:
+        out = append(
+          out,
+          combo(registers, program[ip+1])%8,
+        )
+      case 6:
+        registers[1] = adv(
+          registers,
+          combo(registers, program[ip+1]),
+        )
+      case 7:
+        registers[2] = adv(
+          registers,
+          combo(registers, program[ip+1]),
+        )
+    }
+
+
+    ip += 2
+  }
+  return out
 }
 
 func main() {
-	lines := get_input()
+	registers, program := get_input()
+
+  out := eval(registers, program)
+
+  for i, reg := range registers {
+    fmt.Printf("reg %d: %d\n", i, reg)
+  }
+
+  if len(out) == 0 {
+    fmt.Printf("empty")
+  }
+  for i, o := range out {
+    fmt.Printf("%s", strconv.Itoa(o))
+    if i < len(out)-1 {
+      fmt.Printf(",")
+    }
+  }
 
 
-	total := 0
-
-	fmt.Printf("total: %d\n", total)
 }
---
+++ day17-2.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func get_input() ([]int, []int) {
+	data, _ := os.ReadFile("day17.input")
+  blocks := strings.Split(string(data), "\n\n")
+  registerStrs := strings.Split(blocks[0], "\n")
+  registers := []int{}
+  for _, s := range registerStrs {
+    splitted := strings.Split(s, ": ")
+    x, _ := strconv.Atoi(splitted[1])
+    registers = append(registers, x)
+  }
+
+  progStr := strings.Split(blocks[1], "\n")[0]
+  progList := strings.Split(progStr, ": ")[1]
+  program := []int{}
+  for _, ps := range strings.Split(progList, ",") {
+    x, _ := strconv.Atoi(ps)
+    program = append(program, x)
+  }
+	return registers, program
+}
+
+func disassemble(program []int) {
+  ops := []string{
+    "adv", "bxl", "bst", "jnz", "bxc", "out", "bdv", "cdv",
+  }
+  fmt.Printf("====ASM====\n")
+  for i := 0; i < len(program); i += 2 {
+    op := ops[program[i]]
+
+    if op == "adv" ||
+      op == "bst" ||
+      op == "out" ||
+      op == "bdv" ||
+      op == "cdv" {
+      switch program[i+1] {
+        case 4:
+          fmt.Printf("%d: %s A\n", i, op)
+        case 5:
+          fmt.Printf("%d: %s B\n", i, op)
+        case 6:
+          fmt.Printf("%d: %s C\n", i, op)
+        default:
+          fmt.Printf("%d: %s %d\n", i, op, program[i+1])
+      }
+    } else {
+      fmt.Printf("%d: %s %d\n", i, op, program[i+1])
+    }
+  }
+  fmt.Printf("====---====\n")
+}
+
+func mangleFromTo(
+  program []int, start, end uint64,
+) ([]uint64) {
+  valid := []uint64{}
+  fmt.Printf(
+    "len: %d; %b %b\n",
+    len(program),
+    start,
+    end,
+  )
+
+
+  expected_b := []uint8{}
+  for _, p := range program {
+    expected_b = append(expected_b, (^(uint8(p))) % 8)
+  }
+  //fmt.Printf("%v\n", expected_b)
+
+  var i uint64
+
+  brute:
+  for i = start; i < end; i++ {
+    var A uint64 = i
+    //fmt.Printf("%v\n", A)
+    for _, b := range expected_b {
+      var B uint8 = uint8((A % 8) ^ 2)
+      var C uint8 = uint8((A / (1 << B)) % 8)
+      if B ^ C != b {
+        continue brute
+      }
+      A = A / 8
+    }
+    fmt.Printf("res: %b\n", i)
+    valid = append(valid, i)
+  }
+  return valid
+}
+
+func mangle(program []int) uint64 {
+  //program = program[len(program)-1:]
+
+  valid := []uint64{0}
+  for l := 1; l <= len(program); l++ {
+    to_run := program[len(program)-l:]
+    new_valid := []uint64{}
+    for _, v := range valid {
+      var start uint64 = uint64(v << 3)
+      var end uint64 = uint64((v+1) << 3)
+      ans := mangleFromTo(to_run, start, end)
+      if len(ans) > 0 {
+        new_valid = append(new_valid, ans...)
+      }
+    }
+    valid = new_valid
+  }
+
+  var min_v uint64 = 0
+  for _, v := range valid {
+    if min_v == 0 || min_v > v {
+      min_v = v
+    }
+  }
+
+  return min_v
+}
+
+func main() {
+	_, program := get_input()
+  disassemble(program)
+  total := mangle(program)
+  fmt.Printf("total: %d\n", total)
+}
+
+// 23798076909441 - too low
