b70d83fa88bfe8dcded062b3d9a93fa6819202ba
notTGY <microcraft47@gmail.com>
2024-12-22 12:28:34 +0300

    day 23, 24, 25 prep

---
+++ day25-1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+  "strconv"
+	"time"
+)
+
+func get_input() []string {
+	data, _ := os.ReadFile("day25.input")
+	lines := strings.Split(string(data), "\n")
+	lines = lines[:len(lines)-1]
+  return lines
+}
+func get_test() []string {
+	data, _ := os.ReadFile("day25.test")
+	lines := strings.Split(string(data), "\n")
+	lines = lines[:len(lines)-1]
+  return lines
+}
+
+func main() {
+	start := time.Now()
+	lines := get_test()
+
+	total := 0
+  for _, _ = range lines {
+    total++
+  }
+
+	fmt.Printf("took: %s; total: %d\n", time.Since(start), total)
+}
