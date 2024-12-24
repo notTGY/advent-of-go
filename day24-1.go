73c0936a991c08a0def4850ec2265d96788c1bf4
notTGY <microcraft47@gmail.com>
2024-12-24 15:53:40 +0300

    day 24

--- day24-1.go
+++ day24-1.go
@@ -3,31 +3,104 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
+	"slices"
 	"strings"
 	"time"
 )
 
-func get_input() []string {
+func get_input() ([]string, []string) {
 	data, _ := os.ReadFile("day24.input")
-	lines := strings.Split(string(data), "\n")
+	blocks := strings.Split(string(data), "\n\n")
+	inputs := strings.Split(blocks[0], "\n")
+	lines := strings.Split(blocks[1], "\n")
 	lines = lines[:len(lines)-1]
-	return lines
+	return inputs, lines
 }
-func get_test() []string {
+func get_test() ([]string, []string) {
 	data, _ := os.ReadFile("day24.test")
-	lines := strings.Split(string(data), "\n")
+	blocks := strings.Split(string(data), "\n\n")
+	inputs := strings.Split(blocks[0], "\n")
+	lines := strings.Split(blocks[1], "\n")
 	lines = lines[:len(lines)-1]
-	return lines
+	return inputs, lines
 }
 
 func main() {
 	start := time.Now()
-	lines := get_test()
+	inputs, lines := get_input()
+
+	wire_values := make(map[string]bool)
+	for _, input := range inputs {
+		splitted := strings.Split(input, ": ")
+		val := true
+		if splitted[1] == "0" {
+			val = false
+		}
+		wire_values[splitted[0]] = val
+	}
+
+	zs := []string{}
+	gates := [][]string{}
+	for _, line := range lines {
+		split_a := strings.Split(line, " -> ")
+		split_b := strings.Split(split_a[0], " ")
+		gate := []string{
+			split_b[0], split_b[1], split_b[2], split_a[1],
+		}
+		gates = append(gates, gate)
+		if strings.HasPrefix(split_a[1], "z") {
+			zs = append(zs, split_a[1])
+		}
+	}
+	slices.Sort(zs)
+
+	for {
+		gottem := true
+		for _, z := range zs {
+			_, ok := wire_values[z]
+			if !ok {
+				gottem = false
+			}
+		}
+		if gottem {
+			break
+		}
+
+		for _, gate := range gates {
+			a := gate[0]
+			b := gate[2]
+			c := gate[3]
+			val_a, ok := wire_values[a]
+			if !ok {
+				continue
+			}
+			val_b, ok := wire_values[b]
+			if !ok {
+				continue
+			}
+			val := true
+			switch gate[1] {
+			case "XOR":
+				val = (val_a || val_b) && !(val_a && val_b)
+			case "OR":
+				val = val_a || val_b
+			case "AND":
+				val = val_a && val_b
+			}
+			wire_values[c] = val
+		}
+	}
 
 	total := 0
-	for _, _ = range lines {
-		total++
+	pow := 1
+	for _, z := range zs {
+		vb, _ := wire_values[z]
+		v := 0
+		if vb {
+			v = 1
+		}
+		total = total + v*pow
+		pow *= 2
 	}
 
 	fmt.Printf("took: %s; total: %d\n", time.Since(start), total)
--- day24-2.go
+++ day24-2.go
@@ -3,32 +3,150 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
+	"slices"
 	"strings"
 	"time"
 )
 
-func get_input() []string {
+func get_input() ([]string, [][]string) {
 	data, _ := os.ReadFile("day24.input")
-	lines := strings.Split(string(data), "\n")
+	blocks := strings.Split(string(data), "\n\n")
+	inputs := strings.Split(blocks[0], "\n")
+	lines := strings.Split(blocks[1], "\n")
 	lines = lines[:len(lines)-1]
-	return lines
+
+	gates := [][]string{}
+	for _, line := range lines {
+		split_a := strings.Split(line, " -> ")
+		split_b := strings.Split(split_a[0], " ")
+		gate := []string{
+			split_b[0], split_b[1], split_b[2], split_a[1],
+		}
+		gates = append(gates, gate)
+	}
+
+	return inputs, gates
 }
-func get_test() []string {
+func get_test() ([]string, [][]string) {
 	data, _ := os.ReadFile("day24.test")
-	lines := strings.Split(string(data), "\n")
+	blocks := strings.Split(string(data), "\n\n")
+	inputs := strings.Split(blocks[0], "\n")
+	lines := strings.Split(blocks[1], "\n")
 	lines = lines[:len(lines)-1]
-	return lines
+
+	gates := [][]string{}
+	for _, line := range lines {
+		split_a := strings.Split(line, " -> ")
+		split_b := strings.Split(split_a[0], " ")
+		gate := []string{
+			split_b[0], split_b[1], split_b[2], split_a[1],
+		}
+		gates = append(gates, gate)
+	}
+
+	return inputs, gates
+}
+
+func find(a, b, op string, gates [][]string) string {
+	for _, gate := range gates {
+		if gate[0] == a && gate[1] == op && gate[2] == b {
+			return gate[3]
+		}
+		if gate[0] == b && gate[1] == op && gate[2] == a {
+			return gate[3]
+		}
+	}
+	return ""
 }
 
 func main() {
 	start := time.Now()
-	lines := get_test()
+	_, gates := get_input()
 
-	total := 0
-	for _, _ = range lines {
-		total++
+	xs := []string{}
+	ys := []string{}
+	zs := []string{}
+	for _, gate := range gates {
+		if strings.HasPrefix(gate[0], "x") &&
+			slices.Index(xs, gate[0]) == -1 {
+			xs = append(xs, gate[0])
+		}
+		if strings.HasPrefix(gate[2], "x") &&
+			slices.Index(xs, gate[2]) == -1 {
+			xs = append(xs, gate[2])
+		}
+		if strings.HasPrefix(gate[2], "y") &&
+			slices.Index(ys, gate[2]) == -1 {
+			ys = append(ys, gate[2])
+		}
+		if strings.HasPrefix(gate[0], "y") &&
+			slices.Index(ys, gate[0]) == -1 {
+			ys = append(ys, gate[0])
+		}
+		if strings.HasPrefix(gate[3], "z") {
+			zs = append(zs, gate[3])
+		}
 	}
+	slices.Sort(xs)
+	slices.Sort(ys)
+	slices.Sort(zs)
 
-	fmt.Printf("took: %s; total: %d\n", time.Since(start), total)
+	var swapped []string
+	var c0 string
+
+	for i, x := range xs {
+		y := ys[i]
+
+		// I DO NOT UNDERSTAND THIS===
+		var m1, n1, r1, z1, c1 string
+
+		// Half adder logic
+		m1 = find(x, y, "XOR", gates)
+		n1 = find(x, y, "AND", gates)
+
+		if c0 != "" {
+			r1 = find(c0, m1, "AND", gates)
+			if r1 == "" {
+				m1, n1 = n1, m1
+				swapped = append(swapped, m1, n1)
+				r1 = find(c0, m1, "AND", gates)
+			}
+
+			z1 = find(c0, m1, "XOR", gates)
+
+			if strings.HasPrefix(m1, "z") {
+				m1, z1 = z1, m1
+				swapped = append(swapped, m1, z1)
+			}
+
+			if strings.HasPrefix(n1, "z") {
+				n1, z1 = z1, n1
+				swapped = append(swapped, n1, z1)
+			}
+
+			if strings.HasPrefix(r1, "z") {
+				r1, z1 = z1, r1
+				swapped = append(swapped, r1, z1)
+			}
+
+			c1 = find(r1, n1, "OR", gates)
+		}
+
+		if strings.HasPrefix(c1, "z") && c1 != "z45" {
+			c1, z1 = z1, c1
+			swapped = append(swapped, c1, z1)
+		}
+
+		if c0 == "" {
+			c0 = n1
+		} else {
+			c0 = c1
+		}
+	}
+	// END===
+
+	slices.Sort(swapped)
+	fmt.Printf("res: %s\n", strings.Join(swapped, ","))
+
+	fmt.Printf("took: %s;\n", time.Since(start))
 }
